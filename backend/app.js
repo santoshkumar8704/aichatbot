4635c5c8d86a67a84370df0eea253d8a387847c6
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-07-06 11:35:41 +0530

    ultra

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const openai = new OpenAI({
 
 const PORT = process.env.PORT || 9090;
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174","https://accenchat.vercel.app/","https://accenchat.onrender.com/"],
+  origin: ["http://localhost:5173", "http://localhost:5174","https://accenchat.vercel.app","https://accenchat.onrender.com/"],
 };
 const app = express();
 app.use(cors(corsOptions));
